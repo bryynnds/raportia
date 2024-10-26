107558aef70bb7009bb642c6635a1aa725accfd8
Yanwari R <yanwari.asd@gmail.com>
2024-10-27 01:07:49 +0700

    Nambah input dll

--- app/routes.py
+++ app/routes.py
@@ -9,20 +9,20 @@ def beranda():
 
 @app.route('/data_siswa')
 def data_siswa():
-    # Logika untuk menampilkan halaman Data Siswa
     return render_template('data_siswa.html')
 
 @app.route('/input')
 def input():
-    # Logika untuk menampilkan halaman Input
     return render_template('input.html')
 
 @app.route('/cetak')
 def cetak():
-    # Logika untuk menampilkan halaman Cetak
     return render_template('cetak.html')
 
 @app.route('/daftar_nilai')
 def daftar_nilai():
-    # Logika untuk menampilkan halaman Daftar Nilai
     return render_template('daftar_nilai.html')
+
+@app.route('/profil')  # Ganti nama fungsi di sini
+def profil():  # Mengubah nama fungsi untuk menghindari konflik
+    return render_template('profil.html')
